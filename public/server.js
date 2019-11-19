4e5fb1118970dc1dcc60f7e9cb0e2c5cd283e9cd
Goldpm <lowedavidadam@gmail.com>
2019-11-19 15:24:00 -0500

    changed route

--- public/server.js
+++ public/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
