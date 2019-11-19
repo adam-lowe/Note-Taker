5f85562e0008ab94462806b63a42f21b0b3b0964
Goldpm <lowedavidadam@gmail.com>
2019-11-19 15:19:23 -0500

    moved server js

---
+++ public/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
