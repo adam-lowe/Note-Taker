b7dc90b8a9fe087c8e336a485b4ff45110f96284
Goldpm <lowedavidadam@gmail.com>
2019-11-20 17:33:32 -0500

    added app.gets

--- server.js
+++ server.js
@@ -11,3 +11,20 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    res.json(notes[id]);
+});
+
